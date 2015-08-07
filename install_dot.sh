0c9b7df0d21ad207e509d78d902f00a51192dc14
Simon Kelly <skelly@dimagi.com>
2015-08-07 09:12:56 +0200

    install git completion

--- install_dot.sh
+++ install_dot.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-FILEROOT="./files"
+confirm () {
+    # call with a prompt string or use a default
+    read -r -p "${1:-Are you sure? [y/N]} " response
+    case $response in
+        [yY][eE][sS]|[yY]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
 
-cd $FILEROOT
-for f in * .[^.]*; do
-	cp -irv "$f" "$HOME/"
-done
+copy_local_files() {
+	FILEROOT="./files"
+
+	cd $FILEROOT
+	for f in * .[^.]*; do
+		cp -irv "$f" "$HOME/"
+	done
+}
+
+install_remote_files() {
+	confirm "Install git completion?" && sudo sh -c "curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > /etc/bash_completion.d/git-completion"
+}
+
+confirm "Install local files?" && copy_local_files
+confirm "Install install remote?" && install_remote_files
